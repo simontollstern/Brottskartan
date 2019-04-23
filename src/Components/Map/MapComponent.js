c9211c18b8213c6236cd3c7b7eed5aa5034b1a8d
simontollstern <simoon909@hotmail.com>
2019-04-23 14:04:20 +0200

    Implemented GMaps with Sweden centered

--- src/Components/Map/MapComponent.js
+++ src/Components/Map/MapComponent.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react'
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 // import style from './MapComponent.module.css'
 
 export class MapComponent extends Component {
   render() {
     return (
-      <Map google={this.props.google} zoom={14}>
-        <Marker onClick={this.onMarkerClick} name={'Current location'} />
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-        </InfoWindow>
+      <Map google={this.props.google} zoom={5.5} initialCenter={{lat: 63.1983366,lng: 17.5671981}}>
+
       </Map>
     )
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyBTDUngu6DGdd00qppeaMUj7RhYuUfFOBI'
+  apiKey: 'AIzaSyBTDUngu6DGdd00qppeaMUj7RhYuUfFOBI',
+  language: 'sv-se'
 })(MapComponent)
