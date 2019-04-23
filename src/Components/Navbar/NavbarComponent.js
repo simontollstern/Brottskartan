a515851fe9ba6985deca46635811b082b44e2272
Jonas Forsberg <jonasf@Jonass-MacBook-Pro.local>
2019-04-23 14:07:25 +0200

    Nav links

--- src/Components/Navbar/NavbarComponent.js
+++ src/Components/Navbar/NavbarComponent.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Link} from "react-router-dom";
 import style from './NavbarComponent.module.css'
 
 class NavbarComponent extends Component {
   render() {
     return (
       <div>
-        nav
+        <ul>
+          <Link to="/brott">Brott</Link>
+          <Link to="/polisstationer">Polisstationer</Link>
+          <Link to="/statistik">Statistik</Link>
+        </ul>
       </div>
     )
   }
--- src/Screens/CrimeScreen.js
+++ src/Screens/CrimeScreen.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import NavBarComponent from '../Components/Navbar/NavbarComponent';
+import NavbarComponent from '../Components/Navbar/NavbarComponent';
 
 class CrimeScreen extends Component {
   render() {
     return (
       <div>
-
+        <NavbarComponent></NavbarComponent>
       </div>
     )
   }
