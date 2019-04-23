0994fc0551565420c60e56583a0f76f2384ceaac
simontollstern <simontollstern@gmail.com>
2019-04-23 14:18:06 +0200

    Update CardComponent.js
    Removed some import

--- src/Components/Card/CardComponent.js
+++ src/Components/Card/CardComponent.js
@@ -1,4 +1,4 @@
-import React, { Component, Link } from 'react'
+import React, { Component } from 'react'
 import NavbarComponent from '../Navbar/NavbarComponent'
 import SearchFuncComponent from '../Search/SearchFuncComponent'
 import style from './CardComponent.module.css'
