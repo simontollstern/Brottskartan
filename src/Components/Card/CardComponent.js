6ea87a4ff175621c84b6572ef0b6ee660e1e6d02
michele byman <michelebyman@gmail.com>
2019-04-23 14:13:02 +0200

    card

--- src/Components/Card/CardComponent.js
+++ src/Components/Card/CardComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Link } from 'react'
 import NavbarComponent from '../Navbar/NavbarComponent'
 import SearchFuncComponent from '../Search/SearchFuncComponent'
 import style from './CardComponent.module.css'
@@ -11,6 +11,7 @@ class CardComponent extends Component {
           <h1 className={style.header}>
             Brottskartan
           </h1>
+          <a className={style.link}>Om oss</a>
         </div>
         <NavbarComponent/>
         <SearchFuncComponent/>
