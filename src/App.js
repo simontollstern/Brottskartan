3bc3fce770b5de01bfd1813a0a70eb38d1af8f62
michele byman <michelebyman@gmail.com>
2019-04-23 12:03:49 +0200

    card setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import CardComponent from '../src/Components/Card/CardComponent'
 
 function App() {
   return (
     <div className="App">
-      
+      <CardComponent>
+
+      </CardComponent>
     </div>
   );
 }
