d448891e4e81fa9e77ffd37a2d9742dadbd13c1f
simontollstern <simoon909@hotmail.com>
2019-04-23 13:39:48 +0200

    Created Google Map

--- src/Components/Map/MapComponent.js
+++ src/Components/Map/MapComponent.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
-import style from './MapComponent.module.css'
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+// import style from './MapComponent.module.css'
 
-class MapComponent extends Component {
+export class MapComponent extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <Map google={this.props.google} zoom={14}>
+        <Marker onClick={this.onMarkerClick} name={'Current location'} />
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
+      </Map>
     )
   }
 }
 
-export default MapComponent;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBTDUngu6DGdd00qppeaMUj7RhYuUfFOBI'
+})(MapComponent)
