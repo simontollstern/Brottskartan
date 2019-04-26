fcd58889f93d032b4dcbcf5c423cdf89ce596f48
Jonas Forsberg <jonasf@Jonass-MacBook-Pro.local>
2019-04-26 11:28:55 +0200

    fixed problems with map

--- src/Components/Search/SearchFuncComponent.js
+++ src/Components/Search/SearchFuncComponent.js
@@ -1,18 +1,8 @@
 import React from 'react'
 import style from './SearchFuncComponent.module.css'
-import { GoogleApiWrapper } from 'google-maps-react';
 
 
 function SearchFuncComponent(props) {
-
-  // const input = document.getElementById('input');
-  // const options = {
-  //   types: ['(cities)'],
-  //   componentRestrictions: {country: 'sv'}
-  // }
-
-  // const google = this.props.google;
-  // const autocomplete = new google.maps.places.Autocomplete(input, options);
 
     return (
       <div>
@@ -22,7 +12,4 @@ function SearchFuncComponent(props) {
 
 }
 
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyBTDUngu6DGdd00qppeaMUj7RhYuUfFOBI',
-  language: 'sv-se'
-})(SearchFuncComponent)
+export default SearchFuncComponent
