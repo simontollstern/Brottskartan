99706d88344b6173d0b5fe2ad751e8aa97f3701c
Michele Byman <42961351+michelebyman@users.noreply.github.com>
2019-04-26 11:11:29 +0200

    Update GmapsComponent.js
    deleted console.log

--- src/Components/gmapstest/GmapsComponent.js
+++ src/Components/gmapstest/GmapsComponent.js
@@ -24,7 +24,6 @@ class GmapsComponent extends Component {
   };
 
   renderMarkers = (map) => {
-    console.log(map)
     for(let crime of this.props.crimes){
       // if crime type ISNT (!trafik) then do this (???)
       let marker = new window.google.maps.Marker({
